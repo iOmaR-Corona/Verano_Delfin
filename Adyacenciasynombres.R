bf3a975bf76b9d730094a59d2ba92b0afe65e9e9
iOmaR-Corona <47954015+iOmaR-Corona@users.noreply.github.com>
2020-08-10 20:00:23 -0500

    Add files via upload

--- Adyacenciasynombres.R
+++ Adyacenciasynombres.R
@@ -1,12 +1,24 @@
 library(data.table)
 #Calculo de adyacencias
 ady<-read.csv("C:/Users/Pride Blue/Desktop/veranodelifn/adyacencias.csv")
-adyacencias2<-capture.output( for (i in municipios2019) {
+munyadymatrix<-matrix(nrow = 50,ncol = 15)
+
+   
+
+ for (i in municipios2019) {
+
   munprin<-filter(ady,boundaries.CVEGEO==i)
-  cat(ady[as.numeric(munprin[1,3:22]),1],"\n")
-})
-munyady<-as.data.frame(adyacencias2)
-write_csv(munyady,"C:/Users/Pride Blue/Desktop/veranodelifn/municipiosyadyacencias.csv")
+  cat(i,"\n")
+  adj<-(ady[as.numeric(munprin[1,3:22]),1])
+  dfadj<-data.frame(i,t(adj))
+  dfadj<-cbind(dfadj,t((integer(15-length(dfadj)))))
+  munyadymatrix[match(i,municipios2019),]<-as.numeric(dfadj)
+
+  
+}
+
+munyadymatrix<-as.data.frame(munyadymatrix)
+write_csv(munyadymatrix,"C:/Users/Pride Blue/Desktop/veranodelifn/municipiosyadyacencias1.csv")
 
 
 
